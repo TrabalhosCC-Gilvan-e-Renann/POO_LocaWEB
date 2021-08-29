ac84e3c8b7963fb3fdd4dbb3012b76ec8acfdb3a
Marcos Renann <marcos.renann.br@gmail.com>
2021-08-29 02:29:13 -0300

    Add Account Setters

--- src/main/java/com/locaweb/locaweb/Classes/Account.java
+++ src/main/java/com/locaweb/locaweb/Classes/Account.java
@@ -47,21 +47,36 @@ public class Account extends Client {
     public String getEmail() {
         return email;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getPass() {
         return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
     }
 
     public String getCPF() {
         return cpf;
     }
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
 
     public String getName() {
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getCard() {
         return numberCard;
+    }
+    public void setCard(String card) {
+        this.numberCard = card;
     }
 
     @Override
