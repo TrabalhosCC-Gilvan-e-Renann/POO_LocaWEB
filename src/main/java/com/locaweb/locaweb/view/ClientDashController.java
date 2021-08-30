fea66a1bb90585ed64af96ce301a44a496b7808c
Marcos Renann <marcos.renann.br@gmail.com>
2021-08-29 23:53:09 -0300

    add PayButton

--- src/main/java/com/locaweb/locaweb/view/ClientDashController.java
+++ src/main/java/com/locaweb/locaweb/view/ClientDashController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -18,6 +19,10 @@ public class ClientDashController implements Initializable {
 
     @FXML
     private Label olaLabel;
+
+    @FXML
+    private Label cardLabel;
+
     @FXML
     private Button closeBtn;
     @FXML
@@ -26,15 +31,35 @@ public class ClientDashController implements Initializable {
     private Button payBtn;
 
     @FXML
+    private AnchorPane contaBloqueada;
+
+    @FXML
+    private AnchorPane pagarConta;
+
+    @FXML
     public void CloseClick(ActionEvent event) {
         Stage stage = (Stage) closeBtn.getScene().getWindow();
         stage.close();
     }
 
     @FXML
-    public void PayClick(ActionEvent event) {
+    public void Pay(ActionEvent event) {
         locaWeb.getContaLogada().PaidMonthly();
+        contaBloqueada.setOpacity(0);
+        pagarConta.setOpacity(0);
         updateButtons();
+    }
+
+    @FXML
+    public void DenyPay(ActionEvent event) {
+        contaBloqueada.setOpacity(1);
+        pagarConta.setOpacity(0);
+    }
+    @FXML
+    public void PayClick(ActionEvent event) {
+        contaBloqueada.setOpacity(0);
+        pagarConta.setOpacity(1);
+        cardLabel.setText("Pagar a Mensalidade usando o cartão "+locaWeb.getContaLogada().getCard()+" ?");
     }
 
     public void setLocaWeb(LocaWEB locaWeb) {
@@ -48,6 +73,10 @@ public class ClientDashController implements Initializable {
         Platform.runLater(() -> {
             olaLabel.setText("Olá, "+locaWeb.getContaLogada().getName());
             updateButtons();
+            if(locaWeb.getContaLogada().accountIsBlocked()){
+                contaBloqueada.setOpacity(1);
+                pagarConta.setOpacity(0);
+            }
         });
     }
 
